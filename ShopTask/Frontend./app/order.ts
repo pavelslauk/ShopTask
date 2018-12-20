647ad359fb2199599436528134d281afe688db47
Pavel Slauk <Pavel.slauk.97@mail.ru>
2018-12-20 14:56:47 +0300

    Try to fix 'Fronfend.'. Delete Frontend

--- ShopTask/Frontend./app/order.ts
+++
@@ -1,8 +0,0 @@
-import 'core-js/es6';
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './order/app.module';
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
